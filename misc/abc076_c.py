200508e8a788703c5456fd6fe4593b32a8260fd7
yamaton <yamaton@gmail.com>
2019-05-17 20:38:26 -0400

    Fix a bug

--- misc/abc076_c.py
+++ misc/abc076_c.py
@@ -1,21 +1,18 @@
 # https://abc076.contest.atcoder.jp/tasks/abc076_c
+#
 
 S = input()
 T = input()
 
-s_rev = "".join(reversed(S))
-t_rev = "".join(reversed(T))
-
-found = False
+candidates = []
 for i in range(len(S) - len(T) + 1):
-    yn = all(s_rev[i + k] == "?" or s_rev[i + k] == t_rev[k] for k in range(len(T)))
+    yn = all(S[i + k] == "?" or S[i + k] == T[k] for k in range(len(T)))
     if yn:
-        s_rev = s_rev[:i] + t_rev + s_rev[i + len(T) :]
-        found = True
-        break
+        s = S[:i] + T + S[i + len(T) :]
+        candidates.append(s)
 
-if found:
-    s = "".join("a" if c == "?" else c for c in reversed(s_rev))
-    print(s)
+if candidates:
+    candidates = ["".join("a" if c == "?" else c for c in s) for s in candidates]
+    print(min(candidates))
 else:
     print("UNRESTORABLE")
