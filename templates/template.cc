eab025a766dcf02f06ec3d80c03adbe6171f9311
yamaton <yamaton@gmail.com>
2019-06-15 23:53:48 -0400

    Update template

--- templates/template.cc
+++ templates/template.cc
@@ -1,22 +1,39 @@
 #include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <deque>
+#include <functional>
 #include <iostream>
 #include <map>
-#include <numeric>
+#include <queue>
 #include <set>
+#include <string>
+#include <tuple>
 #include <vector>
 using namespace std;
 
-#define rep(i, n) for (int i = 0; i < n; ++i)
-#define watch(x) cout << (#x) << " is " << (x) << endl
+#define REP(i, n) for (int i = 0; i < n; ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define PB(x) push_back(x)
+#define EB(x) emplace_back(x)
+#define DEBUG(x) cout << (#x) << ": " << (x) << endl
+#define DEBUG_VEC(v)                \
+    cerr << #v << ": ";             \
+    REP(__i, (v).size())            \
+        cerr << ((v)[__i]) << ", "; \
+    cerr << endl
 
 typedef long long ll;
-typedef pair<int, int> ii;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 typedef vector<int> vi;
-typedef vector<ii> vii;
-
-int H, W;
+typedef vector<ll> vl;
 
 int main()
 {
     ios::sync_with_stdio(false);
+
+    return 0;
 }
