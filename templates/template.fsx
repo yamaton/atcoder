58e5504875fe3ad0132b3247c93d640f7205c275
yamato matsuoka <yamaton@gmail.com>
2019-06-28 00:48:03 -0400

    Update template

--- templates/template.fsx
+++ templates/template.fsx
@@ -1,4 +1,12 @@
-open System
-let readInt () = stdin.ReadLine() |> int
-let readIntArray () = stdin.ReadLine().Split(' ') |> Array.map int
+let readInt() = stdin.ReadLine() |> int
+let readIntArray() = stdin.ReadLine().Split(' ') |> Array.map int
 
+let format tf =
+    if tf then "Yes"
+    else "No"
+
+let unwrapOr (alt: 'a) (wrapped: 'a option) =
+    match wrapped with
+    | Some x -> x
+    | None -> alt
+ 
