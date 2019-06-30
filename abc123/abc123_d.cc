347757a3a5570853f803fb26d0ade9e9b4997a8f
yamato matsuoka <yamaton@gmail.com>
2019-06-30 01:23:50 -0400

    Review ABC 123

--- abc123/abc123_d.cc
+++ abc123/abc123_d.cc
@@ -1,3 +1,7 @@
+// K ln (K) implementation in getting top K of elements from
+// cartesian product of three arrays X Y Z
+//
+
 #include <algorithm>
 #include <bitset>
 #include <cassert>
@@ -19,11 +23,11 @@ using namespace std;
 #define REP(i, n) for (int i = 0; i < (n); ++i)
 #define ALL(x) (x).begin(), (x).end()
 #define DEBUG(x) cout << (#x) << ": " << (x) << endl
-#define DEBUG_VEC(v)            \
-    cerr << #v << ": ";         \
-    REP(__i, (v).size())        \
-    cerr << ((v)[__i]) << ", "; \
-    cerr << endl
+#define DEBUG_VEC(v)          \
+  cerr << #v << ": ";         \
+  REP(__i, (v).size())        \
+  cerr << ((v)[__i]) << ", "; \
+  cerr << endl
 
 typedef long long ll;
 typedef pair<int, int> pii;
@@ -31,41 +35,51 @@ typedef pair<ll, ll> pll;
 typedef vector<int> vi;
 typedef vector<ll> vl;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    int x, y, z, k;
-    cin >> x >> y >> z >> k;
-    vl a(x);
-    vl b(y);
-    vl c(z);
-    REP(i, x) cin >> a[i];
-    REP(i, y) cin >> b[i];
-    REP(i, z) cin >> c[i];
+int main() {
+  ios::sync_with_stdio(false);
+  int x, y, z, k;
+  cin >> x >> y >> z >> k;
+  vl a(x);
+  vl b(y);
+  vl c(z);
+  REP(i, x) cin >> a[i];
+  REP(i, y) cin >> b[i];
+  REP(i, z) cin >> c[i];
+  sort(a.begin(), a.end(), greater<ll>());
+  sort(b.begin(), b.end(), greater<ll>());
+  sort(c.begin(), c.end(), greater<ll>());
+  priority_queue<tuple<ll, ll, ll, ll>> q;
+  set<tuple<ll, ll, ll>> seen;
 
-    priority_queue<ll> q;
-    for (auto aa: a) {
-        for (auto bb: b) {
-            q.push(aa + bb);
-        }
+  q.push(make_tuple(a[0] + b[0] + c[0], 0, 0, 0));
+  seen.insert(make_tuple(0, 0, 0));
+
+  ll ans = 0LL;
+  REP(p, k) {
+    ll v, i, j, k;
+    tie(v, i, j, k) = q.top();
+    cout << v << endl;
+    q.pop();
+    tuple<ll, ll, ll> t;
+
+    t = make_tuple(i + 1, j, k);
+    if (seen.find(t) == seen.end() and i + 1 < x) {
+      seen.insert(t);
+      q.push(make_tuple(a[i + 1] + b[j] + c[k], i + 1, j, k));
     }
 
-    priority_queue<ll> q2;
-    int cnt = 0;
-    while (!q.empty() and cnt < k) {
-        ll ab = q.top();
-        q.pop();
-        cnt++;
-        for (auto cc: c) {
-            q2.push(ab + cc);
-        }
+    t = make_tuple(i, j + 1, k);
+    if (seen.find(t) == seen.end() and j + 1 < y) {
+      seen.insert(t);
+      q.push(make_tuple(a[i] + b[j + 1] + c[k], i, j + 1, k));
     }
 
-    REP(i, k) {
-        ll ans = q2.top();
-        q2.pop();
-        cout << ans << endl;
+    t = make_tuple(i, j, k + 1);
+    if (seen.find(t) == seen.end() and k + 1 < z) {
+      seen.insert(t);
+      q.push(make_tuple(a[i] + b[j] + c[k + 1], i, j, k + 1));
     }
+  }
 
-    return 0;
+  return 0;
 }
