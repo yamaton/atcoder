bc27f84f44dbf40804fbfa66da63dfde73836ed4
yamaton <yamaton@gmail.com>
2019-07-10 16:40:07 -0400

    Minor fix

--- abs/fsharp/abc088_b.fsx
+++ abs/fsharp/abc088_b.fsx
@@ -3,13 +3,15 @@ open System
 let readInt() = stdin.ReadLine() |> int
 let readIntArray() = stdin.ReadLine().Split(' ') |> Array.map int
 let n = readInt()
-let xs = readIntArray() |> Array.sortDescending
+let xs = readIntArray()
 
 let alice =
     xs
-    |> Array.indexed
-    |> Array.filter (fun (i, v) -> i % 2 = 0)
-    |> Array.sumBy (fun (i, v) -> v)
+    |> Array.sortDescending
+    |> Array.toSeq
+    |> Seq.indexed
+    |> Seq.filter (fun (i, v) -> i % 2 = 0)
+    |> Seq.sumBy (fun (i, v) -> v)
 
 let bob = Array.sum xs - alice
 
