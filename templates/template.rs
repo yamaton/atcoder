433677b0c0f36a9919c18884eb8b8f5784e39de5
yamaton <yamaton@gmail.com>
2019-07-10 16:39:24 -0400

    Add Rust template

---
+++ templates/template.rs
@@ -0,0 +1,21 @@
+// https://qiita.com/tubo28/items/e6076e9040da57368845
+
+use std::io::*;
+use std::str::FromStr;
+
+fn read<T: FromStr>() -> T {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let token: String = stdin
+        .bytes()
+        .map(|c| c.expect("failed to read char") as char)
+        .skip_while(|c| c.is_whitespace())
+        .take_while(|c| !c.is_whitespace())
+        .collect();
+    token.parse().ok().expect("failed to parse token")
+}
+
+fn main() {
+    let n: i32 = read();
+    let a = (0..n).map(|_| read::<u32>()).collect();
+}
