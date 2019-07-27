7408d624f445e7773bb61b521f53e945088ea88f
yamaton <yamaton@gmail.com>
2019-07-27 11:30:44 -0400

    Update binomial coefficient

--- util/rust/binomial_coefficient.rs
+++ util/rust/binomial_coefficient.rs
@@ -1,49 +1,57 @@
-const BIGINT: u64 = 1_000_000_007;
+const BIGINT: i64 = 1e9 as i64 + 7;
 
-fn pow(mut x: u64, n: u64) -> u64 {
-    let mut ans: u64 = 1;
-    let mut nn = n;
-    let mut k = 0;
-    while nn > 0 {
-        nn = nn >> 1;
-        k += 1
-    }
-    for i in 0..k {
-        if (n >> i) & 1 > 0 {
-            ans = (ans * x) % BIGINT;
-        }
-        x = (x * x) % BIGINT;
+
+fn length_in_base2(x: i64) -> i64 {
+    let mut x = x;
+    let mut ans = 0;
+    while x > 0 {
+        ans += 1;
+        x >>= 1;
     }
     ans
 }
 
-fn modinv(a: u64, p: u64) -> u64 {
-    pow(a, p - 2)
+fn powmod(x: i64, y: i64, z: i64) -> i64 {
+    let mut ans = 1_i64;
+    let mut px = x;
+    let y_size = length_in_base2(y);
+    for i in 0..y_size {
+        if (y >> i) & 1 == 1 {
+            ans = (ans * px) % z;
+        }
+        px = (px * px) % z;
+    }
+    ans
 }
 
-fn binomial_coeff(mut n: u64, r: u64) -> u64 {
+fn modinv(a: i64, p: i64) -> i64 {
+    powmod(a, p - 2, p)
+}
+
+fn binomial_coeff(n: i64, r: i64, z:i64) -> i64 {
     if n < r {
         return 0;
     }
     if 2 * r > n {
-        return binomial_coeff(n, n - r);
+        return binomial_coeff(n, n - r, z);
     }
 
     let mut numer = 1;
     let mut denom = 1;
-    let mut k = r;
+    let mut n = n;
+    let mut r = r;
     for _ in 0..r {
-        numer = (numer * n) % BIGINT;
-        denom = (denom * k) % BIGINT;
+        numer = (numer * n) % z;
+        denom = (denom * r) % z;
         n -= 1;
-        k -= 1;
+        r -= 1;
     }
-    let ans = (numer * modinv(denom, BIGINT)) % BIGINT;
+    let ans = (numer * modinv(denom, z)) % z;
     ans
 }
 
 fn main() {
-    assert_eq!(binomial_coeff(5, 2), 10);
-    assert_eq!(binomial_coeff(64, 32), 114221638);
-    assert_eq!(binomial_coeff(100, 20), 926116413);
+    assert_eq!(binomial_coeff(5, 2, BIGINT), 10);
+    assert_eq!(binomial_coeff(64, 32, BIGINT), 114221638);
+    assert_eq!(binomial_coeff(100, 20, BIGINT), 926116413);
 }
